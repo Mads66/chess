abdd1a976c4fc5edb5f47f01733829d0b50c2d5f
Mads66 <146299742+Mads66@users.noreply.github.com>
2024-09-26 11:46:48 -0600

    Adding incomplete getBoard functionality corrected

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,7 +10,7 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessGame {
-    private ChessBoard myBoard = new ChessBoard(ChessPiece[9][9]);
+    private ChessBoard myBoard = new ChessBoard();
     public ChessGame() {
 
     }
