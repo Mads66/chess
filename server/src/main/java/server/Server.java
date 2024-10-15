fd65ed1d3a61f7f07124cfaed9c79b3652451df7
Mads66 <146299742+Mads66@users.noreply.github.com>
2024-10-15 10:38:12 -0600

    Adding(register) hard coded implementation

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,32 @@
+package server;
+
+import spark.*;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+        Spark.post("/user", (req, res) -> createUser(req, res));
+
+
+        //This line initializes the server and can be removed once you have a functioning endpoint 
+        Spark.init();
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    private String createUser(Request req, Response res) {
+        return """
+                [200] { "username":"", "authToken":"" }""";
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+}
