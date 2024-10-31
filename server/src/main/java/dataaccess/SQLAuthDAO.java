d49838b914c31274edd8f7f4dd427fbb82c793b0
Mads66 <146299742+Mads66@users.noreply.github.com>
2024-10-31 12:02:02 -0600

    Adding passed SQLUserDAO tests

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -21,7 +21,7 @@ public class SQLAuthDAO implements AuthDAO {
               `authToken` varchar(256) NOT NULL,
               `username` varchar(256) NOT NULL,
               `json` TEXT DEFAULT NULL,
-              PRIMARY KEY (`id`),
+              PRIMARY KEY (`authToken`),
               INDEX(username)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
--- server/src/test/java/dataaccess/tests/DataAccessTest.java
+++ server/src/test/java/dataaccess/tests/DataAccessTest.java
@@ -1,6 +1,5 @@
 package dataaccess.tests;
 
-import dataaccess.MemoryUserDAO;
 import dataaccess.SQLAuthDAO;
 import dataaccess.SQLGameDAO;
 import dataaccess.SQLUserDAO;
@@ -8,7 +7,9 @@ import exception.ResponseException;
 import model.UserData;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DataAccessTest {
     private final SQLUserDAO userDAO = new SQLUserDAO();
@@ -26,4 +27,36 @@ public class DataAccessTest {
         var actual = userDAO.getUser(expected);
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void registerSameUser() throws Exception {
+        var repeat = new UserData("a", "p", "a@a.com");
+        assertThrows(ResponseException.class, () -> userDAO.createUser(repeat));
+    }
+
+    @Test
+    public void getUser() throws Exception {
+        var original = new UserData("a", "p", "a@a.com");
+        var db_user = userDAO.getUser(original);
+        assertEquals(original, db_user);
+    }
+
+    @Test
+    public void badGetUser() throws Exception {
+        var notInDatabase = new UserData("imNotReal", "plainAsDay", "fake@person.com");
+        assertNull(userDAO.getUser(notInDatabase));
+    }
+
+    @Test
+    public void clearUser() throws Exception {
+        userDAO.createUser(new UserData("b", "pass", "b@b.com"));
+        userDAO.createUser(new UserData("c", "password", "c@c.com"));
+        userDAO.createUser(new UserData("d", "password", "d@d.com"));
+        userDAO.clear();
+        assertNull(userDAO.getUser(new UserData("a", "p", "a@a.com")));
+        assertNull(userDAO.getUser(new UserData("b", "pass", "b@b.com")));
+        assertNull(userDAO.getUser(new UserData("c", "password", "c@c.com")));
+    }
+
+
 }
