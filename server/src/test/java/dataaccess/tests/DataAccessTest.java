976bee21cba2c53c8b7157686d62566c40cde791
Mads66 <146299742+Mads66@users.noreply.github.com>
2024-10-31 11:44:36 -0600

    Adding first DAO test

--- server/src/test/java/dataaccess/tests/DataAccessTest.java
+++ server/src/test/java/dataaccess/tests/DataAccessTest.java
@@ -1,18 +1,29 @@
 package dataaccess.tests;
 
 import dataaccess.MemoryUserDAO;
+import dataaccess.SQLAuthDAO;
+import dataaccess.SQLGameDAO;
+import dataaccess.SQLUserDAO;
+import exception.ResponseException;
 import model.UserData;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DataAccessTest {
+    private final SQLUserDAO userDAO = new SQLUserDAO();
+    private final SQLGameDAO gameDAO = new SQLGameDAO();
+    private final SQLAuthDAO authDAO = new SQLAuthDAO();
+
+    public DataAccessTest() throws ResponseException {
+    }
+
+
     @Test
     public void registerUser() throws Exception {
-        var dataAccess = new MemoryUserDAO();
         var expected = new UserData("a", "p", "a@a.com");
-        dataAccess.createUser(expected);
-        var actual = dataAccess.getUser(expected);
+        userDAO.createUser(expected);
+        var actual = userDAO.getUser(expected);
         assertEquals(expected, actual);
     }
 }
